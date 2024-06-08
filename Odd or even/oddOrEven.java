e1d665e92c2fd91d365f8a94741cdf240bf41bc3
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-08 07:36:50 +0000

    Day 12

---
+++ Odd or even/oddOrEven.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class oddOrEven{
+
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int x = in.nextInt();
+
+        String sign;
+        String odd_or_even;
+        String zero;
+
+        if(x == 0){
+            zero = "zero";
+        }else{
+            zero = "";
+        }
+
+        if(x%2==0){
+            odd_or_even ="even";
+        }else{
+            odd_or_even = "odd";
+        }
+
+        if(x >= 0){
+            sign = "positive";
+        }else if(x < 0){
+            sign = "negative";
+        }else{
+            sign = "positive";
+        }
+
+        if(zero == "zero"){
+            System.out.println("The number is 0");
+        }else{
+            System.out.println(x + " is a " + sign + " " + odd_or_even + " number");
+        }
+
+        
+
+
+    }
+}
