7a28100933244f5261db14fabfbac769fb509d92
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-08 07:59:33 +0000

    Day 12

---
+++ Palindrome/Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter string: ");
+        String word = in.nextLine();
+
+        StringBuilder sb_word = new StringBuilder(word);
+        
+        if(word.equals(sb_word.reverse().toString())){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+        
+
+    }//main end
+}//class end
