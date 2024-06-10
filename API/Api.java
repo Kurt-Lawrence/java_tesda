d3ac74bba134385747f925578798a09aff01ef37
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-10 11:59:21 +0800

    Update Api.java
    in pc

--- API/Api.java
+++ API/Api.java
@@ -3,6 +3,11 @@ package API;
 
 public class Api{
 
-    
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("kurt");
+
+        System.out.println(sb.toString().toUpperCase());
+    }
     
 }
