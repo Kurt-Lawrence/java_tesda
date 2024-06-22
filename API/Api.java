54b81352503aa7f7f7db4d6a21dd13244e305992
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-22 06:20:25 +0000

    Output

--- API/Api.java
+++ API/Api.java
@@ -6,8 +6,15 @@ package API;
 public class Api{
 
     public static void main(String[] args) {
+
+        cumulativeSum(5,2);
         
-         
     }
+
+    static void cumulativeSum(int... nums){
+
+        for(int num:nums){
+            System.out.println(num);
+        }
     
-}
+}}
