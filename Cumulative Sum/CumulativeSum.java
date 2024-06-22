2d36b97f4510204d519662ad601d4a3d199a14cf
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-22 06:13:17 +0000

    output

---
+++ Cumulative Sum/CumulativeSum.java
@@ -0,0 +1,16 @@
+
+public class CumulativeSum{
+    public static void main(String[] args) {
+
+        cumulativeSum(5,2);
+        
+    }
+
+    static void cumulativeSum(int... nums){
+
+        for(int num:nums){
+            System.out.println(num);
+        }
+
+    }
+}
